f964fe153b86c1a8b342eba33ed0ce0e7eaf456e
Rachel Sipes <rasipes@gmail.com>
2020-03-08 15:24:42 -0400

    added connection to mysql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ("Blackbean", 0);
-INSERT INTO burgers (burger_name, devoured) VALUES ("Morningstar", 0);
-INSERT INTO burgers (burger_name, devoured) VALUES ("Impossible", 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Blackbean", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Morningstar", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Impossible", FALSE);
