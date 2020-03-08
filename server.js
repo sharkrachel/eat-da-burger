a4d83ac7172c56154df3672eac1e4d348baec368
Rachel Sipes <rasipes@gmail.com>
2020-03-08 15:17:55 -0400

    successfully connected to the server

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
 var express = require("express");
-// var mysql = require("mysql");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+var exphbs = require("express-handlebars");
+
+var app = express();
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+app.use(methodOverride("_method"));
+app.engine("handlebars", exphbs({
+    defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controller");
+app.use("/", routes);
 
 var PORT = process.env.PORT || 3000;
+app.listen(PORT);
 
