e80965f39d59fada85cd7936081e7664df2aedb3
Rachel Sipes <rasipes@gmail.com>
2020-03-28 16:16:02 -0400

    changed connection file to work with heroku

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,21 @@
 var mysql = require("mysql");
-var connection = mysql.createConnection({
-    host: 'i943okdfa47xqzpy.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 
-    user: 'tqhx72ocx9jodip1', 
-    password: 'dj2r00gwxopfighz',
-    database: 'burgers_db'
+var connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "Snoopdogg1!",
+        database: "burgers_db"
+    });
+}
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
 });
-
-connection.connect(function(err){
-    if(err)throw err;
-    console.log("connected as id: " + connection.threadId);
-});
-
 module.exports = connection;
